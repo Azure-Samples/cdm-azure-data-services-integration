1b589eae7b67e417488a104c651421380860bced
Premal Shah <preshah@microsoft.com>
2018-12-02 16:58:31 +0000

    Update read-write-demo-wide-world-importers.py
    Made changes to take in parameters.

--- AzureDatabricks/Samples/read-write-demo-wide-world-importers.py
+++ AzureDatabricks/Samples/read-write-demo-wide-world-importers.py
@@ -1,6 +1,23 @@
 # Databricks notebook source
+dbutils.widgets.removeAll()
+dbutils.widgets.text("inputCDMFolderLocation", "https://cdsabyosadev01dxt.dfs.core.windows.net/powerbi/WideWorldImporters-1/WideWorldImporters-Orders/model.json", "InputCDMFolderLocation")
+dbutils.widgets.text("outputCDMFolderLocation", "https://cdsabyosadev01dxt.dfs.core.windows.net/powerbi/MatthewTest/wideworldimportersdemo","OutputCDMFolderLocation")
+inputLocation = dbutils.widgets.get("inputCDMFolderLocation")
+outputLocation = dbutils.widgets.get("outputCDMFolderLocation")
+
+if inputLocation == '':
+  inputLocation = "https://cdsabyosadev01dxt.dfs.core.windows.net/powerbi/WideWorldImporters-1/WideWorldImporters-Orders/model.json"
+
+if outputLocation == '':
+  outputLocation = "https://cdsabyosadev01dxt.dfs.core.windows.net/powerbi/PremalTest/wideworldimportersdemo"
+
+print(inputLocation)
+print(outputLocation)
+
+# COMMAND ----------
+
 salesOrderDf = (spark.read.format("com.microsoft.cdm")
-                    .option("cdmModel", "https://cdsabyosadev01dxt.dfs.core.windows.net/powerbi/WideWorldImporters-1/WideWorldImporters-Orders/model.json")
+                    .option("cdmModel", inputLocation)
                     .option("entity", "Sales Orders")
                     .option("appId", "bd8e91da-c58c-4407-95e0-a8ecad012fc7")
                     .option("appKey", "Lky5pTg2teniHdbeTlmOuivjdIPBPcwMQJ49wGwKiXA=")
@@ -10,7 +27,7 @@ salesOrderDf = (spark.read.format("com.microsoft.cdm")
 # COMMAND ----------
 
 salesOrderLinesDf = (spark.read.format("com.microsoft.cdm")
-                     .option("cdmModel", "https://cdsabyosadev01dxt.dfs.core.windows.net/powerbi/WideWorldImporters-1/WideWorldImporters-Orders/model.json")
+                     .option("cdmModel", inputLocation)
                      .option("entity", "Sales OrderLines")
                      .option("appId", "bd8e91da-c58c-4407-95e0-a8ecad012fc7")
                      .option("appKey", "Lky5pTg2teniHdbeTlmOuivjdIPBPcwMQJ49wGwKiXA=")
@@ -20,7 +37,7 @@ salesOrderLinesDf = (spark.read.format("com.microsoft.cdm")
 # COMMAND ----------
 
 salesCustomerDf = (spark.read.format("com.microsoft.cdm")
-                     .option("cdmModel", "https://cdsabyosadev01dxt.dfs.core.windows.net/powerbi/WideWorldImporters-1/WideWorldImporters-Orders/model.json")
+                     .option("cdmModel", inputLocation)
                      .option("entity", "Sales Customers")
                      .option("appId", "bd8e91da-c58c-4407-95e0-a8ecad012fc7")
                      .option("appKey", "Lky5pTg2teniHdbeTlmOuivjdIPBPcwMQJ49wGwKiXA=")
@@ -30,7 +47,7 @@ salesCustomerDf = (spark.read.format("com.microsoft.cdm")
 # COMMAND ----------
 
 salesCustomerCategoriesDf = (spark.read.format("com.microsoft.cdm")
-                     .option("cdmModel", "https://cdsabyosadev01dxt.dfs.core.windows.net/powerbi/WideWorldImporters-1/WideWorldImporters-Orders/model.json")
+                     .option("cdmModel", inputLocation)
                      .option("entity", "Sales CustomerCategories")
                      .option("appId", "bd8e91da-c58c-4407-95e0-a8ecad012fc7")
                      .option("appKey", "Lky5pTg2teniHdbeTlmOuivjdIPBPcwMQJ49wGwKiXA=")
@@ -40,7 +57,7 @@ salesCustomerCategoriesDf = (spark.read.format("com.microsoft.cdm")
 # COMMAND ----------
 
 salesBuyingGroupsDf = (spark.read.format("com.microsoft.cdm")
-                     .option("cdmModel", "https://cdsabyosadev01dxt.dfs.core.windows.net/powerbi/WideWorldImporters-1/WideWorldImporters-Orders/model.json")
+                     .option("cdmModel", inputLocation)
                      .option("entity", "Sales BuyingGroups")
                      .option("appId", "bd8e91da-c58c-4407-95e0-a8ecad012fc7")
                      .option("appKey", "Lky5pTg2teniHdbeTlmOuivjdIPBPcwMQJ49wGwKiXA=")
@@ -50,7 +67,7 @@ salesBuyingGroupsDf = (spark.read.format("com.microsoft.cdm")
 # COMMAND ----------
 
 warehouseStockItemsDf = (spark.read.format("com.microsoft.cdm")
-                              .option("cdmModel", "https://cdsabyosadev01dxt.dfs.core.windows.net/powerbi/WideWorldImporters-1/WideWorldImporters-Orders/model.json")
+                              .option("cdmModel", inputLocation)
                               .option("entity", "Warehouse StockItems")
                               .option("appId", "bd8e91da-c58c-4407-95e0-a8ecad012fc7")
                               .option("appKey", "Lky5pTg2teniHdbeTlmOuivjdIPBPcwMQJ49wGwKiXA=")
@@ -60,7 +77,7 @@ warehouseStockItemsDf = (spark.read.format("com.microsoft.cdm")
 # COMMAND ----------
 
 warehouseColorsDf = (spark.read.format("com.microsoft.cdm")
-                          .option("cdmModel", "https://cdsabyosadev01dxt.dfs.core.windows.net/powerbi/WideWorldImporters-1/WideWorldImporters-Orders/model.json")
+                          .option("cdmModel", inputLocation)
                           .option("entity", "Warehouse Colors")
                           .option("appId", "bd8e91da-c58c-4407-95e0-a8ecad012fc7")
                           .option("appKey", "Lky5pTg2teniHdbeTlmOuivjdIPBPcwMQJ49wGwKiXA=")
@@ -70,7 +87,7 @@ warehouseColorsDf = (spark.read.format("com.microsoft.cdm")
 # COMMAND ----------
 
 warehousePackageTypesDf = (spark.read.format("com.microsoft.cdm")
-                                .option("cdmModel", "https://cdsabyosadev01dxt.dfs.core.windows.net/powerbi/WideWorldImporters-1/WideWorldImporters-Orders/model.json")
+                                .option("cdmModel", inputLocation)
                                 .option("entity", "Warehouse PackageTypes")
                                 .option("appId", "bd8e91da-c58c-4407-95e0-a8ecad012fc7")
                                 .option("appKey", "Lky5pTg2teniHdbeTlmOuivjdIPBPcwMQJ49wGwKiXA=")
@@ -85,12 +102,26 @@ warehousePackageTypesDf = (spark.read.format("com.microsoft.cdm")
 
 # COMMAND ----------
 
+salesBuyingGroupsDf.printSchema()
+
+# COMMAND ----------
+
 display(salesBuyingGroupsDf)
 
 # COMMAND ----------
 
-unassignedBuyingGroupDf = spark.createDataFrame([(-1, 'Unassigned', 0, '2013-01-01', '9999-12-31')])
+from pyspark.sql.types import StructType, StructField, LongType, StringType, DateType
+from pyspark.sql.functions import to_date, lit
+
+unassignedBuyingGroupDf = spark.sql("select -1, 'Unassigned', 0, to_date('2013-01-01 00:00:00.0000000', 'yyyy-MM-dd H:mm:ss.SSSSSSS'), to_date('9999-12-31 23:59:59', 'yyyy-MM-dd H:mm:ss')")
+
 newSalesBuyingGroupsDf = salesBuyingGroupsDf.union(unassignedBuyingGroupDf)
+
+display(newSalesBuyingGroupsDf)
+
+# COMMAND ----------
+
+newSalesBuyingGroupsDf.printSchema()
 
 # COMMAND ----------
 
@@ -145,6 +176,16 @@ newSalesCustomerDf.printSchema()
 
 # COMMAND ----------
 
+from pyspark.sql.functions import concat
+
+hashedSalesCustomerDf = (newSalesCustomerDf.withColumn("ChangeTrackingHash", 
+                                                       concat(newSalesCustomerDf.BuyingGroupID.cast("String"),
+                                                              newSalesCustomerDf.CreditLimit.cast("String"))))
+
+display(hashedSalesCustomerDf)
+
+# COMMAND ----------
+
 from pyspark.sql.functions import md5
 
 hashedSalesCustomerDf = newSalesCustomerDf.withColumn("ChangeTrackingHash", hash_function_udf(newSalesCustomerDf.BuyingGroupID, newSalesCustomerDf.CustomerID))
@@ -169,7 +210,7 @@ salesCustomerCategoriesDf.createOrReplaceTempView("salesCustomerCategories")
 
 # COMMAND ----------
 
-corporateSalesCustomerDf = spark.sql("select * from hashedSalesCustomer c, salesCustomerCategories cc where c.customerCategoryID = cc.customerCategoryID and cc.CustomerCategoryName = 'Corporate'")
+corporateSalesCustomerDf = spark.sql("select * from hashedSalesCustomer c, salesCustomerCategories cc where c.customerCategoryID = cc.customerCategoryID and cc.CustomerCategoryName != 'Corporate'")
 
 # COMMAND ----------
 
@@ -188,7 +229,7 @@ display(corporateSalesCustomerDf)
              .option("appId", "bd8e91da-c58c-4407-95e0-a8ecad012fc7")
              .option("appKey", "Lky5pTg2teniHdbeTlmOuivjdIPBPcwMQJ49wGwKiXA=")
              .option("tenantId", "72f988bf-86f1-41af-91ab-2d7cd011db47")
-             .option("cdmFolder", "https://cdsabyosadev01dxt.dfs.core.windows.net/powerbi/MatthewTest/wideworldimportersdemo")
+             .option("cdmFolder", outputLocation)
              .option("cdmModelName", "Transformed-Wide-World-Importers")
              .save())
 
@@ -199,7 +240,7 @@ display(corporateSalesCustomerDf)
              .option("appId", "bd8e91da-c58c-4407-95e0-a8ecad012fc7")
              .option("appKey", "Lky5pTg2teniHdbeTlmOuivjdIPBPcwMQJ49wGwKiXA=")
              .option("tenantId", "72f988bf-86f1-41af-91ab-2d7cd011db47")
-             .option("cdmFolder", "https://cdsabyosadev01dxt.dfs.core.windows.net/powerbi/MatthewTest/wideworldimportersdemo")
+             .option("cdmFolder", outputLocation)
              .option("cdmModelName", "Transformed-Wide-World-Importers")
              .save())
 
@@ -210,7 +251,7 @@ display(corporateSalesCustomerDf)
              .option("appId", "bd8e91da-c58c-4407-95e0-a8ecad012fc7")
              .option("appKey", "Lky5pTg2teniHdbeTlmOuivjdIPBPcwMQJ49wGwKiXA=")
              .option("tenantId", "72f988bf-86f1-41af-91ab-2d7cd011db47")
-             .option("cdmFolder", "https://cdsabyosadev01dxt.dfs.core.windows.net/powerbi/MatthewTest/wideworldimportersdemo")
+             .option("cdmFolder", outputLocation)
              .option("cdmModelName", "Transformed-Wide-World-Importers")
              .save())
 
@@ -221,7 +262,7 @@ display(corporateSalesCustomerDf)
              .option("appId", "bd8e91da-c58c-4407-95e0-a8ecad012fc7")
              .option("appKey", "Lky5pTg2teniHdbeTlmOuivjdIPBPcwMQJ49wGwKiXA=")
              .option("tenantId", "72f988bf-86f1-41af-91ab-2d7cd011db47")
-             .option("cdmFolder", "https://cdsabyosadev01dxt.dfs.core.windows.net/powerbi/MatthewTest/wideworldimportersdemo")
+             .option("cdmFolder", outputLocation)
              .option("cdmModelName", "Transformed-Wide-World-Importers")
              .save())
 
@@ -232,7 +273,7 @@ display(corporateSalesCustomerDf)
              .option("appId", "bd8e91da-c58c-4407-95e0-a8ecad012fc7")
              .option("appKey", "Lky5pTg2teniHdbeTlmOuivjdIPBPcwMQJ49wGwKiXA=")
              .option("tenantId", "72f988bf-86f1-41af-91ab-2d7cd011db47")
-             .option("cdmFolder", "https://cdsabyosadev01dxt.dfs.core.windows.net/powerbi/MatthewTest/wideworldimportersdemo")
+             .option("cdmFolder", outputLocation)
              .option("cdmModelName", "Transformed-Wide-World-Importers")
              .save())
 
@@ -243,7 +284,7 @@ display(corporateSalesCustomerDf)
              .option("appId", "bd8e91da-c58c-4407-95e0-a8ecad012fc7")
              .option("appKey", "Lky5pTg2teniHdbeTlmOuivjdIPBPcwMQJ49wGwKiXA=")
              .option("tenantId", "72f988bf-86f1-41af-91ab-2d7cd011db47")
-             .option("cdmFolder", "https://cdsabyosadev01dxt.dfs.core.windows.net/powerbi/MatthewTest/wideworldimportersdemo")
+             .option("cdmFolder", outputLocation)
              .option("cdmModelName", "Transformed-Wide-World-Importers")
              .save())
 
@@ -254,7 +295,7 @@ display(corporateSalesCustomerDf)
              .option("appId", "bd8e91da-c58c-4407-95e0-a8ecad012fc7")
              .option("appKey", "Lky5pTg2teniHdbeTlmOuivjdIPBPcwMQJ49wGwKiXA=")
              .option("tenantId", "72f988bf-86f1-41af-91ab-2d7cd011db47")
-             .option("cdmFolder", "https://cdsabyosadev01dxt.dfs.core.windows.net/powerbi/MatthewTest/wideworldimportersdemo")
+             .option("cdmFolder", outputLocation)
              .option("cdmModelName", "Transformed-Wide-World-Importers")
              .save())
 
@@ -265,6 +306,6 @@ display(corporateSalesCustomerDf)
              .option("appId", "bd8e91da-c58c-4407-95e0-a8ecad012fc7")
              .option("appKey", "Lky5pTg2teniHdbeTlmOuivjdIPBPcwMQJ49wGwKiXA=")
              .option("tenantId", "72f988bf-86f1-41af-91ab-2d7cd011db47")
-             .option("cdmFolder", "https://cdsabyosadev01dxt.dfs.core.windows.net/powerbi/MatthewTest/wideworldimportersdemo")
+             .option("cdmFolder", outputLocation)
              .option("cdmModelName", "Transformed-Wide-World-Importers")
              .save())
